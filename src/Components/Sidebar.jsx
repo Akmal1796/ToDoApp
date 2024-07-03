ec2e751b19a607cf5c82e3799cf0986a530ee82a
Akmal1796 <mhdakmal1796@gmail.com>
2024-07-03 11:43:13 +0530

    Added the profile image

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Sidebar.css'
+import profile from '/public/Images/profile.jpg'
 
 const Sidebar = () => {
   return (
     <div>
         <div>
-            <img />
+            <img src={profile} className='profile-img'/>
             <p>Mohamed Akmal</p>
             <p>abcd@gmail.com</p>
         </div>
