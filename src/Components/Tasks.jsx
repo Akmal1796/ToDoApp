da5f5ae57be9478db8d56de872ff9bf6032eab44
Akmal1796 <mhdakmal1796@gmail.com>
2024-07-03 22:55:05 +0530

    Created the basic structure for the todo card

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Tasks.css'
 import { FaRegClipboard } from "react-icons/fa6";
 import { LuPlus } from "react-icons/lu";
+import { PiDotsThreeOutlineLight } from "react-icons/pi";
 
 const Tasks = () => {
   return (
@@ -24,9 +25,24 @@ const Tasks = () => {
         </div>
 
         <div className='todo-container'>
-          <div className='todo-header'></div>
-          <div className='todo-content'></div>
-          <div className='todo-status'></div>
+          <div className='todo-header'>
+            <h3>Complete The Landing Page UI Design</h3>
+            <div><PiDotsThreeOutlineLight size={20} color='gray'/></div>
+          </div>
+          <div className='todo-content'>
+            <p>Get the work done before 4'O clock with complete responsive design</p>
+          </div>
+          <div className='todo-status'>
+            <div>
+              <p>Priority: <span>Moderate</span></p>
+            </div>
+            <div>
+              <p>Status: <span>Not Started</span></p>
+            </div>
+            <div>
+              <p>Created On: <span>03/07/2024</span></p>
+            </div>
+          </div>
         </div>
 
       </div>
