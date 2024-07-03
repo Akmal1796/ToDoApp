49b023c3c8270a1476350328f1ce4434521d8cab
Akmal1796 <mhdakmal1796@gmail.com>
2024-07-03 15:22:54 +0530

    Completed the designing of the sidebar section

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -5,8 +5,8 @@ import { MdDashboard } from "react-icons/md";
 import { FaExclamation } from "react-icons/fa";
 import { BiTask } from "react-icons/bi";
 import { BsListTask } from "react-icons/bs";
-import { IoMdSettings } from "react-icons/io";
-import { IoMdHelpCircle } from "react-icons/io";
+import { IoMdSettings, IoMdHelpCircle } from "react-icons/io";
+import { MdLogout } from 'react-icons/md';
 
 const Sidebar = () => {
   return (
@@ -18,32 +18,38 @@ const Sidebar = () => {
                 <p>abcd@gmail.com</p>
             </div>
         </div>
-        <ul className='sidebar-items'>
-            <li className='active'>
-                <MdDashboard className='sidebar-icons'/>
-                <div>Dashboard</div>
-            </li>
-            <li>
-                <FaExclamation className='sidebar-icons'/>
-                <div>Vital Task</div>
-            </li>
-            <li>
-                <BiTask className='sidebar-icons'/>
-                <div>My Task</div>
-            </li>
-            <li>
-                <BsListTask className='sidebar-icons'/>
-                <div>Task Categories</div>
-            </li>
-            <li>
-                <IoMdSettings className='sidebar-icons'/>
-                <div>Settings</div>
-            </li>
-            <li>
-                <IoMdHelpCircle className='sidebar-icons'/>
-                <div>Help</div>
-            </li>
-        </ul>
+        <div className='sidebar-container'>
+            <ul className='sidebar-menu-items'>
+                <li className='active'>
+                    <MdDashboard className='sidebar-icons'/>
+                    <div>Dashboard</div>
+                </li>
+                <li>
+                    <FaExclamation className='sidebar-icons'/>
+                    <div>Vital Task</div>
+                </li>
+                <li>
+                    <BiTask className='sidebar-icons'/>
+                    <div>My Task</div>
+                </li>
+                <li>
+                    <BsListTask className='sidebar-icons'/>
+                    <div>Task Categories</div>
+                </li>
+                <li>
+                    <IoMdSettings className='sidebar-icons'/>
+                    <div>Settings</div>
+                </li>
+                <li>
+                    <IoMdHelpCircle className='sidebar-icons'/>
+                    <div>Help</div>
+                </li>
+            </ul>
+            <div className='logout-btn-container'>
+                <MdLogout className='sidebar-icons'/>
+                <div>Logout</div>
+            </div>
+        </div>
     </div>
   )
 }
