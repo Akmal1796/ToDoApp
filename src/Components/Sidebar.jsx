8f4f2f2d33f00b44b27469e277689b98ef447957
Akmal1796 <mhdakmal1796@gmail.com>
2024-07-03 11:23:46 +0530

    Started Working on the sidebar

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+        <div>
+            <img />
+            <p>Mohamed Akmal</p>
+            <p>abcd@gmail.com</p>
+        </div>
+        <ul>
+            <li>Dashboard</li>
+            <li>Vital Task</li>
+            <li>My Task</li>
+            <li>Task Categories</li>
+            <li>Settings</li>
+            <li>Help</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Sidebar
