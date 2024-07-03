35387f8e4fd55a91a21d3c7d67bfad8ea1fa1164
Akmal1796 <mhdakmal1796@gmail.com>
2024-07-03 22:23:20 +0530

    Started the working on the task section

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import './Tasks.css'
+import { FaRegClipboard } from "react-icons/fa6";
+import { LuPlus } from "react-icons/lu";
 
 const Tasks = () => {
   return (
     <div className='tasks-container'>
+
       <div className='todo'>
-        Hello
+        <div className='todo-header'>
+
+          <div className='todo-header-left-section'>
+            <div className='header-left'><FaRegClipboard size={25} color='gray'/>
+            <span> To-Do</span></div>
+            <div className='header-date'>03 July <span>・ Today</span></div>
+          </div>
+
+          <div className='todo-header-right-section'>
+            <div className='header-right'><LuPlus size={25} className='plus-icon'/><span> Add Task</span></div>
+          </div>
+
+        </div>
       </div>
+
       <div className='task-status'>
         World
       </div>
