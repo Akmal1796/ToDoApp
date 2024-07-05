d3276d6aa32c992ad6cb2ea64b43002063f351ff
Akmal1796 <mhdakmal1796@gmail.com>
2024-07-05 22:57:15 +0530

    Fixed a big bug

--- src/Components/VitalTask.jsx
+++ src/Components/VitalTask.jsx
@@ -11,64 +11,64 @@ import { PiChartPieSliceFill } from "react-icons/pi";
 
 const VitalTask = () => {
   return (
-    <div className='tasks-container'>
-      <div className='todo'>
+    <div className='vital-tasks-container'>
+      <div className='vital-todo'>
         <div>
 
-          <div className='todo-header'>
-            <div className='heading'>Vital Tasks</div>
-            <div className='underline'></div>
+          <div className='vital-todo-header'>
+            <div className='vital-heading'>Vital Tasks</div>
+            <div className='vital-underline'></div>
           </div>
 
-          <div className='todo-container'>
-            <div className='todo-content-header'>
-              <div className='todo-bullet-header'>
-                <div className="outer-circle">
-                  <div className="inner-circle"></div>
+          <div className='vital-todo-container'>
+            <div className='vital-todo-content-header'>
+              <div className='vital-todo-bullet-header'>
+                <div className="vital-outer-circle">
+                  <div className="vital-inner-circle"></div>
                 </div>
                 <h3>Complete The Landing Page UI Design</h3>
               </div>
               <div><PiDotsThreeOutlineLight size={20} color='gray'/></div>
             </div>
-            <div className='todo-content'>
+            <div className='vital-todo-content'>
               <p>Get the work done before 4'O clock with complete responsive design</p>
-              <img src={UIDesign} className='todo-img'/>
+              <img src={UIDesign} className='vital-todo-img'/>
             </div>
-            <div className='todo-status'>
-              <div className='status-priority'>
+            <div className='vital-todo-status'>
+              <div className='vital-status-priority'>
                 <p>Priority: <span>Moderate</span></p>
               </div>
-              <div className='status'>
+              <div className='vital-status'>
                 <p>Status: <span>Not Started</span></p>
               </div>
-              <div className='status-date'>
+              <div className='vital-status-date'>
                 <p>Created On: 03/07/2024</p>
               </div>
             </div>
           </div>
 
-          <div className='todo-container'>
-            <div className='todo-content-header'>
-              <div className='todo-bullet-header'>
-                <div className="outer-circle-progress">
-                  <div className="inner-circle"></div>
+          <div className='vital-todo-container'>
+            <div className='vital-todo-content-header'>
+              <div className='vital-todo-bullet-header'>
+                <div className="vital-outer-circle-progress">
+                  <div className="vital-inner-circle"></div>
                 </div>
                 <h3>Start the new project on food store</h3>
               </div>
               <div><PiDotsThreeOutlineLight size={20} color='gray'/></div>
             </div>
-            <div className='todo-content'>
+            <div className='vital-todo-content'>
               <p>Starts the works on time because of the tight schedules for upcoming days. At least complete the requirements gatheirng within 1 week period.</p>
-              <img src={Project} className='todo-img'/>
+              <img src={Project} className='vital-todo-img'/>
             </div>
-            <div className='todo-status'>
-              <div className='status-priority'>
+            <div className='vital-todo-status'>
+              <div className='vital-status-priority'>
                 <p>Priority: <span>Moderate</span></p>
               </div>
-              <div className='status-progress'>
+              <div className='vital-status-progress'>
                 <p>Status: <span>In Progress</span></p>
               </div>
-              <div className='status-date'>
+              <div className='vital-status-date'>
                 <p>Created On: 03/07/2024</p>
               </div>
             </div>
@@ -78,7 +78,7 @@ const VitalTask = () => {
 
       </div>
 
-      <div className='completed-tasks-container'>
+      <div className='vital-completed-tasks-container'>
       </div>
     </div>
   )
