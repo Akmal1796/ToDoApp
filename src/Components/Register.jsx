2b8de940afea2db6e4ee67c23b25c246d1e22f70
Akmal1796 <mhdakmal1796@gmail.com>
2024-07-07 23:35:59 +0530

    Completed the signup page left section

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import './Register.css'
+import IMG from '/public/Images/register-img.png'
+import { IoPersonSharp } from "react-icons/io5";
+import { MdLock } from "react-icons/md";
+import { IoMdMail } from "react-icons/io";
+import { MdPersonAddAlt1 } from "react-icons/md";
+import { MdPersonAddAlt } from "react-icons/md";
+import { MdOutlineLock } from "react-icons/md";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+    return (
+        <div className='login-page'>
+            <div className='login-container'>
+    
+                <div className='login-left-section'>
+                    <img src={IMG} className='login-img'/>
+                </div>
+    
+                <div className='login-right-section'>
+                    <h1>Sign In</h1>
+                    <form>
+                        <div className="input-container">
+                            <i className="icon-class"><IoPersonSharp size={30} color='black'/></i>
+                            <input type="text" placeholder="Enter Username" className='input-box'/>
+                        </div>
+                        <div className="input-container">
+                            <i className="icon-class"><MdLock size={30} color='black'/></i>
+                            <input type="text" placeholder="Enter Password" className='input-box'/>
+                        </div>
+                        <div className='remeber-me-section'>
+                            <input type='checkbox' />
+                            <label>Remeber me</label>
+                        </div>
+                        <div>
+                            <button type='submit' className='login-btn'>Login</button>
+                        </div>
+                    </form>
+    
+                    <div className='login-brand-container'>
+                        <div className='login-with-brands-account'>
+                        </div>
+                        <p>Don't have an account? <a href=''>Create One</a></p>                   
+                    </div>
+                </div>
+            </div>
+        </div>
+      )
 }
 
 export default Register
