2fdee699d2b3d82fdd98c50a2e9a3c30e050af07
Akmal1796 <mhdakmal1796@gmail.com>
2024-07-07 21:39:58 +0530

    Created the basic structure for the login form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import './Login.css'
 import IMG from '/public/Images/login-img.png'
+import { IoPersonSharp } from "react-icons/io5";
+import { MdLock } from "react-icons/md";
 
 const Login = () => {
   return (
     <div className='login-page'>
         <div className='login-container'>
-            <div className='login-left-section'></div>
+
+            <div className='login-left-section'>
+                <h1>Sign In</h1>
+                <form>
+                    <div class="input-container">
+                        <i class="icon-class"><IoPersonSharp size={30} color='black'/></i>
+                        <input type="text" placeholder="Enter Username" />
+                    </div>
+                    <div class="input-container">
+                        <i class="icon-class"><MdLock size={30} color='black'/></i>
+                        <input type="text" placeholder="Enter Password" />
+                    </div>
+                    <div>
+                        <label>Remeber me</label>
+                        <input type='checkbox' />
+                    </div>
+                    <div>
+                        <button type='submit'>Login</button>
+                    </div>
+                </form>
+            </div>
+
             <div className='login-right-section'>
                 <img src={IMG} className='login-img'/>
             </div>
